5717726b0f5a7bdba12326314eea1611b933cf06
xiaoxx <me@xiaoxx.cc>
2019-12-15 17:31:29 +0800

    解决下载问题，以及没填生日导致错位的问题

--- README.md
+++ README.md
@@ -1,42 +1,159 @@
 # 微信公众号会员信息导出
-[点这里安装](https://github.com/xiaoxx970/wechat_vip_info_export/raw/master/Wechat_offical_account_VIP_info_export.user.js)（需要浏览器已经安装Tampermonkey）
+推荐使用谷歌浏览器[Chrome](https://www.google.cn/intl/zh-CN/chrome/)安装插件[Tampermonkey](https://www.tampermonkey.net/?ext=dhdg&locale=zh)使用，其他浏览器没测试过
 
-> Tampermonkey，中文名叫油猴，是一个装在浏览器上的插件，一般浏览器如Edge、Chrome、Firefox都可以安装，官网在这：[Tampermonkey官网](https://www.tampermonkey.net/?ext=dhdg&locale=zh)，安装后打开：[脚本页面](https://github.com/xiaoxx970/wechat_vip_info_export/raw/master/Wechat_offical_account_VIP_info_export.user.js)，点击`Install this script`就可以安装我的脚本，接下来打开会员详情页就可以开始自动运作
+## 功能
 
-脚本功能：导出微信公众号后台的所有会员卡信息，格式为csv，可以直接复制进Excel里面，方便其他平台导入。
+导出你能看到的会员的所有信息，看起来像这个样子
 
-在微信公众号的`卡劵功能->会员卡-> 会员管理`界面使用，脚本会自动从第一个会员详情点开，把会员信息保存到浏览器的localstorage后返回，继续点开下一个。
+| 会员  | 地区            | 姓名 | 手机        | 生日       | 状态 | 是否关注 | 标签   | 会员号         | 积分 |
+| ----- | --------------- | ---- | ----------- | ---------- | ---- | -------- | ------ | -------------- | ---- |
+| lucky | 中国  江苏 苏州 | 张三 | 13700000000 | 1999-9-9   | 有效 | 否       | 无标签 | 9492-3564-9589 | 11   |
+| 👧     | 毛里求斯        | 赵四 | 13800000000 | 2000-10-10 | 无效 | 已关注   | 无标签 | 2391-8300-1593 | 30   |
 
-最好在前台运行，打开调试工具后可以在console看到当前正在导出本页的第几个会员信息。
+## 步骤
 
-到最后一页的时候就把localstorage里的信息作为文件让浏览器下载。
+### 如果你是资深玩家
 
-你始终可以通过浏览器右上角tampermonkey按钮终止脚本运行
-终止后要想从头开始导入，需要先清除cookie和localstorage数据，在console输入：
+用过油猴插件，那么直接[点这里打开安装页面](https://github.com/xiaoxx970/wechat_vip_info_export/raw/master/Wechat_offical_account_VIP_info_export.user.js)。
 
-```js
-function clearCookie(c_name) {
-    var expiredays = -1
-    var exdate = new Date()
-    exdate.setDate(exdate.getDate() + expiredays)
-    document.cookie = c_name + "=" + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
-}
-localStorage.removeItem("csv")
-clearCookie("step")
-```
+在微信公众号的`卡劵功能->会员卡-> 会员管理`界面就可以使用。
+
+### 如果你没听说过Tampermonkey
+
+那么你需要：
+
+1. 下载安装谷歌浏览器：https://www.google.cn/intl/zh-CN/chrome/
+
+2. 下载Tampermonkey插件
+
+   去这个网站：https://www.crx4chrome.com/crx/755/
+
+   ![image-20191215145542390](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215145542390.png)
+
+   点其中一个链接（除了google cdn 和 Chrome web store），下载插件
+
+3. 解压插件
+
+   下载后你得到的是一个crx格式的文件，重命名文件修改后缀，把`.crx`改成`.zip`
+
+   ![image-20191215150212447](md-img/image-20191215150212447.png)=>![image-20191215150325561](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215150325561.png)
+
+   解压zip文件：点解压到"extension_xxx\”
+
+   ![image-20191215150624537](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215150624537.png)
+
+   记住这个文件夹的位置
+
+4. 安装插件
+
+   打开Chrome浏览器，在地址栏输入`chrome://extensions/`，或者点浏览器菜单按钮 > 更多工具 > 扩展程序
+
+   ![image-20191215151002595](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215151002595.png)
+
+   进入扩展程序页面，打开右上角开发者模式开关，点左边的`加载已解压的扩展程序`
+
+   ![image-20191215151145803](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215151145803.png)
+
+   选择刚才解压出来的文件夹（不需要点进去，选中文件夹后就点`选择文件夹`）
+
+   然后就可以安装好插件
+
+   ![image-20191215151317653](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215151317653.png)
+
+   看到这个就说明安装好了
+
+5. 安装我的脚本
+
+   打开脚本页面：[微信公众号会员信息导出](https://greasyfork.org/zh-CN/scripts/387221-微信公众号会员信息导出-wechat-offical-account-vip-info-export)
+
+   点`安装此脚本`
+
+   ![image-20191215151923867](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215151923867.png)
+
+   然后在这个页面点安装，点一下就可以安装好
+
+6. 开始做事情
+
+   打开微信公众号的`卡劵功能->会员卡-> 会员管理`界面
+
+   ![image-20191215152613213](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215152613213.png)只要一进入会员管理页面，浏览器就会开始自动跳转，收集信息。然后就等着吧。
+
+   平均一个会员需要4秒导出，如果九百个会员的话需要一个小时。
+
+7. 下载导出的csv文件
+
+   如果脚本执行完最后一页的最后一个用户，就会出现提示说导出完成，可以开始下载
+
+   ![image-20191215153051694](https://xiaoxx.oss-cn-beijing.aliyuncs.com/md-img/image-20191215153051694.png)
+
+   下载下来的`vip.csv`文件可以通过Excel打开，完事。
+
+## 注意事项
+
+- 打开调试工具后可以在console看到当前正在导出本页的第几个会员信息。
+
+- 你始终可以通过浏览器右上角tampermonkey按钮终止脚本运行
+  
+- 任何时候你都可以把当前已经导出的会员先下载下来
+
+  停止脚本后在console输入
+
+  ```javascript
+  function getDownloadUrl(csvData) {
+      var _utf = "\uFEFF"; // 为了使Excel以utf-8的编码模式，同时也是解决中文乱码的问题
+      if (window.Blob && window.URL && window.URL.createObjectURL) {
+          var csvData = new Blob([_utf + csvData], {
+              type: 'text/csv'
+          });
+          return URL.createObjectURL(csvData);
+      }
+  }
+  function SaveAs(fileName, csvData) {
+      var Sys = {};
+      var ua = navigator.userAgent.toLowerCase();
+      var s;
+      (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1]:0;	//这里直接指定了chrome
+      var bw = Sys;
+      var alink = document.createElement("a");
+      alink.id = "linkDwnldLink";
+      alink.href = getDownloadUrl(csvData);
+      document.body.appendChild(alink);
+      var linkDom = document.getElementById('linkDwnldLink');
+      linkDom.setAttribute('download', fileName);
+      linkDom.click();
+      document.body.removeChild(linkDom);
+  }
+  var csv = localStorage.getItem("csv")
+  csv = "会员,地区,姓名,手机,生日,状态,是否关注,标签,会员号,积分\n"+csv
+  SaveAs("vip.csv", csv)
+  ```
+  
+- 终止后要想从头开始导入（或者从某一页），需要先清除cookie和localstorage数据，在console输入：
+
+    ```js
+  function clearCookie(c_name) {
+      var expiredays = -1
+      var exdate = new Date()
+      exdate.setDate(exdate.getDate() + expiredays)
+      document.cookie = c_name + "=" + ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
+  }
+  localStorage.removeItem("csv")
+  clearCookie("step")
+  ```
+
+- 在会员页面打开插件后不会马上开始运行脚本，要刷新一下才可以
 
 现在还有的问题：
-- 导出的会员有没写生日的，会出现错位的情况
-    解决方法：在Excel里面排序后把没有生日的往又拖一格就可以了
-- 导出结束后可能会停在最后一页，不下载csv文件
-    解决方法：打开console手动运行这三行：
-    ```js
-    var csv = localStorage.getItem("csv")
-    csv = "会员,地区,姓名,手机,生日,浏览器状态,是否关注,标签,会员号,积分\n"+csv
-    window.open("data:text/csv;charset=utf-8,"+csv)
-    ```
+
+- 有时候会停下来不继续点下一个页面，这时候看一下console里的提示信息
+
+  - 如果写的`next page`，那只要手动点一下下一页就可以了
+
+  - 如果写的`step:`后面跟一个数字，那数字是几就手动点一下当前页面的第几个会员信息
+
+  - 如果停在某个会员信息页，那可以手动返回上一页试试
 ---
 
-> Note:仅仅是为我自己做的脚本，如果没达到你的要求，欢迎找我有尝定制。QQ：987896425
+> Note:仅仅是为我自己做的脚本，如果没达到你的要求，欢迎找我有尝定制。（比如你还需要导出会员卡使用次数、激活时间、使用记录等）QQ：987896425
 
 > 要是你有觉得可以改进的地方，欢迎来创建Pull request。
--- Wechat_offical_account_VIP_info_export.user.js
+++ Wechat_offical_account_VIP_info_export.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         微信公众号会员信息导出[Wechat_offical_account_VIP_info_export]
 // @namespace    https://github.com/xiaoxx970/wechat_vip_info_export
-// @version      0.8
+// @version      0.9
 // @description  导出微信公众号的所有会员卡信息，保存为csv格式
 // @author       Xiaoxx
 // @match        https://mp.weixin.qq.com/merchant/membercardmgr?*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
     function getCookie(c_name) {
         if (document.cookie.length > 0) {
@@ -22,68 +22,33 @@
         }
         return ""
     }
-    function setCookie(c_name,value) {
+    function setCookie(c_name, value) {
         var expiredays = 365
         var exdate = new Date()
         exdate.setDate(exdate.getDate() + expiredays)
         document.cookie = c_name + "=" + escape(value) +
             ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString())
     }
-    function read_info(){
+    function read_info() {
         var info = document.getElementsByClassName("msg_pre_view")[0].innerText
-        var info_array = info.split("\n")//转换信息为数组
-        info_array[1] = info_array[1].replace(" ","")//删掉会员名后面小笔的图标
+        var info_array = info.split("\n") // 转换信息为数组
+        info_array[1] = info_array[1].replace(" ", "") // 删掉会员名后面小笔的图标
+        info_array[info_array.length - 3] = info_array[info_array.length - 3].replace("发消息", "") // 删除按钮标签里的文字
         var info_es = []
         var info_head = []
-        for (var i=0;i<8;i++){
-            info_head[i]=info_array.splice(0,1)[0];//偶数行为head
-            info_es[i]=info_array.splice(0,1)[0]; //奇数行为内容
+        var info_no = info_array["length"] / 2
+        for (var i = 0; i < info_no; i++) {
+            info_head[i] = info_array.splice(0, 1)[0]; // 偶数行为head
+            info_es[i] = info_array.splice(0, 1)[0]; // 奇数行为内容
         }
+        if (info_head[4] != "生日") // 解决没填生日导致错位的问题
+            info_es.splice(4, 0, "无")
         var card_no = document.getElementsByClassName("msg_pre_view")[1].getElementsByTagName("span")[1].innerText
         var card_point = parseInt(document.getElementsByClassName("msg_pre_view")[1].getElementsByTagName("span")[2].innerText)
-        info_head.push("会员号","积分")
-        info_es.push(card_no,card_point)
+        info_head.push("会员号", "积分")
+        info_es.push(card_no, card_point)
         var csv = info_es.join(",")
         return csv
-    }
-    function SaveAs(fileName, csvData) {
-        var bw = browser();
-        if(!bw['edge'] || !bw['ie']) {
-            var alink = document.createElement("a");
-            alink.id = "linkDwnldLink";
-            alink.href = getDownloadUrl(csvData);
-            document.body.appendChild(alink);
-            var linkDom = document.getElementById('linkDwnldLink');
-            linkDom.setAttribute('download', fileName);
-            linkDom.click();
-            document.body.removeChild(linkDom);
-        }
-        else if(bw['ie'] >= 10 || bw['edge'] == 'edge') {
-            var _utf = "\uFEFF";
-            var _csvData = new Blob([_utf + csvData], {
-                type: 'text/csv'
-            });
-            navigator.msSaveBlob(_csvData, fileName);
-        }
-        else {
-            var oWin = window.top.open("about:blank", "_blank");
-            oWin.document.write('sep=,\r\n' + csvData);
-            oWin.document.close();
-            oWin.document.execCommand('SaveAs', true, fileName);
-            oWin.close();
-        }
-    }
-    function browser() {
-        var Sys = {};
-        var ua = navigator.userAgent.toLowerCase();
-        var s;
-        (s = ua.indexOf('edge') !== - 1 ? Sys.edge = 'edge' : ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1]:
-        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
-        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
-        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
-        (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
-        (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
-        return Sys;
     }
     function getDownloadUrl(csvData) {
         var _utf = "\uFEFF"; // 为了使Excel以utf-8的编码模式，同时也是解决中文乱码的问题
@@ -93,56 +58,70 @@
             });
             return URL.createObjectURL(csvData);
         }
-        // return 'data:attachment/csv;charset=utf-8,' + _utf + encodeURIComponent(csvData);
     }
-    window.addEventListener('load', function() {
+    function SaveAs(fileName, csvData) { // 保存scv文件用
+        var Sys = {};
+        var ua = navigator.userAgent.toLowerCase();
+        var s;
+        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] : 0; // 这里直接指定了chrome
+        var bw = Sys;
+        var alink = document.createElement("a");
+        alink.id = "linkDwnldLink";
+        alink.href = getDownloadUrl(csvData);
+        document.body.appendChild(alink);
+        var linkDom = document.getElementById('linkDwnldLink');
+        linkDom.setAttribute('download', fileName);
+        linkDom.click();
+        document.body.removeChild(linkDom);
+    }
+    window.addEventListener('load', function () {
         var step = parseInt(getCookie("step"))
-        if(window.location.search.has("action=user_list")){//分支1：在会员列表页
-            if (getCookie("step")==""){
-                setCookie("step","1");
+        if (window.location.search.has("action=user_list")) { // 分支1：在会员列表页
+            if (getCookie("step") == "") {
                 step = 1;
+                setCookie("step", step);
             }
-            if(step>10){
-                step=1
-                setCookie("step",step)
+            if (step > 10) { // 去往下一页
+                step = 1;
+                setCookie("step", step);
                 try {
-                    document.getElementById("js_next_page").click()//去往下一页
+                    document.getElementById("js_next_page").click()
                 }
-                catch(err) {
+                catch (err) {
                     alert("导出完成，下载csv文件")
                     var csv = localStorage.getItem("csv")
-                    csv = "会员,地区,姓名,手机,生日,状态,是否关注,标签,会员号,积分\n"+csv
+                    csv = "会员,地区,姓名,手机,生日,状态,是否关注,标签,会员号,积分\n" + csv
                     SaveAs("vip.csv", csv)
                 }
                 console.log("next page")
-                return//这个return必须要否则下一页的点击速度赶不上去详情页的速度
+                return // 这个return必须要否则下一页的点击速度赶不上去详情页的速度
             }
-            console.log("step:"+step)
-            setCookie("back_url",window.location.href)
+            console.log("step:" + step)
+            setCookie("back_url", window.location.href)
             try {
-                window.location.href=document.getElementsByClassName("tbody")[0].getElementsByTagName("a")[step*2-1].href//去往详情页
+                window.location.href = document.getElementsByClassName("tbody")[0].getElementsByTagName("a")[step * 2 - 1].href//去往详情页
             }
-            catch(err){
+            catch (err) {
                 alert("导出完成，下载csv文件")
                 var csv = localStorage.getItem("csv")
-                csv = "会员,地区,姓名,手机,生日,状态,是否关注,标签,会员号,积分\n"+csv
+                csv = "会员,地区,姓名,手机,生日,状态,是否关注,标签,会员号,积分\n" + csv
                 SaveAs("vip.csv", csv)
             }
             return
         }
-        if(window.location.search.has("action=user_detail")){//分支2：在会员详情页
+        if (window.location.search.has("action=user_detail")) { // 分支2：在会员详情页
             step++;
-            setCookie("step",step)
+            setCookie("step", step)
             /*在这里做事情*/
             var csv = localStorage.getItem("csv")
-            if (csv == null){
+            if (csv == null) {
                 csv = ""
-                localStorage.setItem("csv",csv)
+                localStorage.setItem("csv", csv)
             }
-            csv = csv+"\n"+read_info()
-            localStorage.setItem("csv",csv)//用localstorage存储csv
+            csv = csv + "\n" + read_info()
+            localStorage.setItem("csv", csv) // 用localstorage存储csv
             console.log("back")
-            window.location.href=getCookie("back_url")
+            window.location.href = getCookie("back_url")
             //window.history.back();//后退
             //return
         }
